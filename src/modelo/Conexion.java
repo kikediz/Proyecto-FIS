44a869002d001930a3a841eac85d3c31810acbfa
kikediz <kikediz@gmail.com>
2015-05-15 14:09:20 -0500

    código con funcionalidad lógica solo del login

---
+++ src/modelo/Conexion.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Conexion {
+    private Login login= new Login();
+    public static Conexion instancia; 
+    private Connection cnn;
+   
+    private Conexion(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            cnn=DriverManager.getConnection("jdbc:postgresql://localhost:5432/c_informatica",login.getUser(),login.getPassword());
+        } catch (ClassNotFoundException ex) {
+            System.out.println("error 1"+ex.getMessage());
+        } catch (SQLException ex) {
+          System.out.println("error 2"+ex.getMessage());
+        }
+    }
+   
+   public synchronized static Conexion entregarConexion() {
+   if(instancia==null)
+   { 
+   instancia=new Conexion();
+   }
+   return instancia;
+   
+   
+   
+   } 
+   public void cerraConexion(){
+   instancia=null;
+   }
+
+    /**
+     * @return the cnn
+     */
+    public Connection getCnn() {
+        return cnn;
+    }
+   
+   
+}
---
+++ src/modelo/Login.java
@@ -0,0 +1,58 @@
+package modelo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author maximiliano
+ */
+public class Login implements Objetos{
+
+    private String user;
+    private String password;
+    private String rol;
+    
+    public Login (){}
+
+    public Login(String user, String password, String Rol) {
+        this.user = user;
+        this.password = password;
+        this.rol = Rol;
+    }
+    
+    
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+    @Override
+    public boolean[] validar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
